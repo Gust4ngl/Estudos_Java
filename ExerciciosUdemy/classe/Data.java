9d22605476a049324f6dc98a5a643812cefa2a43
Gust4ngl <gustavosilvaskt@outlook.com>
2021-12-26 16:10:13 -0300

    Testando construtores e metodos

--- ExerciciosUdemy/classe/Data.java
+++ ExerciciosUdemy/classe/Data.java
@@ -2,4 +2,21 @@ package classe;
 
 public class Data {
 	int dia, mes, ano;
+			
+	Data(){
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	Data(int diaIncial, int mesInicial, int anoInicial){
+		dia = diaIncial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
+	
+	String obterDataformatada() {
+		return String.format("Data: %d/%d/%d",dia, mes, ano);
+	}
+	
 }
--- ExerciciosUdemy/classe/DataTeste.java
+++ ExerciciosUdemy/classe/DataTeste.java
@@ -3,20 +3,12 @@ package classe;
 public class DataTeste {
 
 	public static void main(String[] args) {
-		Data diaAleatorio = new Data();
 		
-		diaAleatorio.dia = 05;
-		diaAleatorio.mes = 12;
-		diaAleatorio.ano = 2019;
+		Data dataCalendario = new Data();
+		System.out.println(dataCalendario.obterDataformatada());
 		
-		System.out.printf("Data: %d/%d/%d", diaAleatorio.dia, diaAleatorio.mes, diaAleatorio.ano);
-		
-		Data natal = new Data();
-		natal.dia = 25;
-		natal.mes = 12;
-		natal.ano = 2021;
-		
-		System.out.printf("\nData: %d/%d/%d", natal.dia, natal.mes, natal.ano);
+		Data natal = new Data(25, 12, 2021);
+		System.out.println(natal.obterDataformatada());
 	}
 
 }

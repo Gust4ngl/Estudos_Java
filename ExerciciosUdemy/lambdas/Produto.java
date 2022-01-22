f512768999c5b7f9028296c2671fe9182c6092a4
Gust4ngl <gustavosilvaskt@outlook.com>
2022-01-22 06:57:30 -0300

    concluindo desafio do modulo lambdas

--- ExerciciosUdemy/lambdas/Produto.java
+++ ExerciciosUdemy/lambdas/Produto.java
@@ -1,10 +1,10 @@
 package lambdas;
 
-public class Produto extends Object{
+public class Produto extends Object {
 
-	final String nome;
-	final double preco;
-	final double desconto;
+	public String nome;
+	public double preco;
+	public double desconto;
 
 	public Produto(String nome, double preco, double desconto) {
 		super();
@@ -12,7 +12,7 @@ public class Produto extends Object{
 		this.preco = preco;
 		this.desconto = desconto;
 	}
-	
+
 	public String toString() {
 		double precoFinal = preco * (1 - desconto);
 		return nome + " tem o preço de: " + precoFinal;

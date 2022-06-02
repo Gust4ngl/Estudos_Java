841243a66fd136e0c2f6cd7a7a9ad50b33d53574
Gust4ngl <gustavosilvaskt@outlook.com>
2022-06-02 18:26:13 -0300

    Aim trainer finalizado

--- Testes-JavaFX/src/jogo/aimtrainer/PainelAlvos.java
+++ Testes-JavaFX/src/jogo/aimtrainer/PainelAlvos.java
@@ -8,32 +8,14 @@ public class PainelAlvos extends GridPane{
 
 	public PainelAlvos() {
 
-		Caixa c1 = new Caixa();
-		Caixa c2 = new Caixa();
-		Caixa c3 = new Caixa();
-		Caixa c4 = new Caixa();
-		Caixa c5 = new Caixa();
-		Caixa c6 = new Caixa();
-		
-		remover(c1);
-		remover(c2);
-		remover(c3);
-		remover(c4);
-		remover(c5);
-		remover(c6);
-		
 		getColumnConstraints().addAll(cc(), cc(), cc(), cc(), cc());
 		getRowConstraints().addAll(rc(), rc(), rc(), rc(), rc());
 		
+		Caixa c1 = new Caixa();
+		Caixa c2 = new Caixa();
 		
-		add(c1, (int) (Math.random() * 5), (int) (Math.random() * 5));
-		add(c2, (int) (Math.random() * 5), (int) (Math.random() * 5));
-		add(c3, (int) (Math.random() * 5), (int) (Math.random() * 5));
-		add(c4, (int) (Math.random() * 5), (int) (Math.random() * 5));
-		add(c5, (int) (Math.random() * 5), (int) (Math.random() * 5));
-		add(c6, (int) (Math.random() * 5), (int) (Math.random() * 5));
-		
-		
+		adicionar(c1);
+		remover(c1);
 		
 	}
 	
@@ -51,10 +33,16 @@ public class PainelAlvos extends GridPane{
 		return rc;
 	}
 	
-	private void remover(Caixa caixa) {
-		caixa.setOnMouseClicked(e -> {
-			getChildren().remove(caixa);
+	private void remover(Caixa c1) {
+		c1.setOnMouseClicked(e -> {
+			getChildren().remove(c1);
+			adicionar(c1);
 		});
 	}
+	
+	private void adicionar(Caixa caixa) {
+		add(caixa, (int) (Math.random() * 5), (int) (Math.random() * 5));
+	}
+	
 	
 }

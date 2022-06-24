f1cd7e723b08fb4a19664dff7fba181c90144b20
Gust4ngl <gustavosilvaskt@outlook.com>
2022-06-24 12:59:14 -0300

    Mais alguns testes usando spring boot

--- ExerciciosUdemy/ExerciciosSB/src/main/java/exercicios/controllers/ClienteController.java
+++ ExerciciosUdemy/ExerciciosSB/src/main/java/exercicios/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package exercicios.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import exercicios.models.Cliente;
@@ -14,5 +16,12 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		return new Cliente(8, "Gusta", "12345678912");
 	}
-	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "João", "123456789");
+	}
+	@GetMapping()
+	public Cliente obterCloentePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Carlos", "1234567823");
+	}
 }
